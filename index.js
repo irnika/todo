e91cc160f25ee9fffa52c5e7bf654858f0e856a6
Irnika <redduna@ukr.net>
2020-04-16 00:21:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 const todos = [
-    { title: "Задача 1", description: "Сделать что-то", completed: false }, // 0
-    { title: "Задача 2", description: "Сделать что-то", completed: true }, // 1
-    { title: "Задача 3", description: "Сделать что-то", completed: false } // 2
- ];
- 
+  { title: "Задача 1", description: "Сделать что-то", completed: false }, // 0
+  { title: "Задача 2", description: "Сделать что-то", completed: true }, // 1
+  { title: "Задача 3", description: "Сделать что-то", completed: false } // 2
+];
+
+console.log("-------1 задание-----------")
+
 function addTodo(title, description) {
-    todos.push({ title: title, description: description, completed: false }) 
-  }
-  
+  todos.push({ title: title, description: description, completed: false }) 
+}
+
 addTodo("Задача 4", "Сделать что-то")
 console.log(todos)
 
-//---------------------
 
+//---------------------
+console.log("-------2 задание-----------")
 
 function toggleTodo(index) {
-    if (todos[index].completed){
-        todos[index].completed = false
-        }
-    else{todos[index].completed = true}
-  }
-  
+  if (todos[index].completed){
+      todos[index].completed = false
+      }
+  else{todos[index].completed = true}
+}
+
 toggleTodo(3)
 console.log(todos)
 
-//---------------------
 
+//---------------------
+console.log("-------3 задание-----------")
 
 function deleteTodo(index) {
-    todos.splice(index, 1)
-  }
+  todos.splice(index, 1)
+}
 
 deleteTodo(1)
 console.log(todos)
 
-//--------------------------
 
+//--------------------------
+console.log("-------4 задание-----------")
 
 function updateTodo(index, todo) {
-  if(todo.title){
-    todos[index].title = todo.title
-  }
-     if(todo.description){
-    todos[index].description = todo.description
-  }  
+if(todo.title){
+  todos[index].title = todo.title
+}
+   if(todo.description){
+  todos[index].description = todo.description
+}  
 }
 
 updateTodo(2, {title: "Новый заголовoк задачи", description: "Новое описание задачи"})
-console.log(todos)
-
-//----------------------
-
 
 console.log(todos)
- 
